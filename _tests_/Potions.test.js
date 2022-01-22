d9f7d9bf5425dafb700a12d0f6741f7dd9010a45
tooqk4u <tooqk4u@protonmail.com>
2022-01-22 15:33:26 -0500

    created repository/directories/files, potions constructor and tests

---
+++ _tests_/Potions.test.js
@@ -0,0 +1,7 @@
+test('creates a random potion object', () => {
+  const potion = new Potion();
+
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
+});
